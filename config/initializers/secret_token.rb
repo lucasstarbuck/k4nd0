fe5fb51df0548fbdcc5c7e8f1797aae9f4ce3876
Lucas Starbuck <lucasstarbuck@gmail.com>
2013-09-02 18:36:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kandu::Application.config.secret_key_base = '76c0fe41eb9960e81d6f7807505e1eae5b7177e169fff9a6693a9f45ab47c9e0078c5b992967c2145fabca9af1cf9a6ff381c100f344255939fcee9e297cab33'
